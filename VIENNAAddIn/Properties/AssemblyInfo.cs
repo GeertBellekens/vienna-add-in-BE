0b3d9257c6be33e59695bd558f6612142bb68c3f
Geert Bellekens <geert@bellekens.com>
2018-06-20 14:07:54 +0200

    version 2.0.0

--- VIENNAAddIn/Properties/AssemblyInfo.cs
+++ VIENNAAddIn/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion ("1.0.3.*")]
+[assembly: AssemblyVersion ("2.0.0.*")]
 [assembly: AssemblyDelaySign (false)]
 [assembly: AssemblyKeyFile ("")]
 [assembly: AssemblyKeyName ("")]
