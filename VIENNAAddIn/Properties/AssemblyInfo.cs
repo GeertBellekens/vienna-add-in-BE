7f82c8f8de6552cf7422748c0fc6ee64685b7403
Geert Bellekens <geert@bellekens.com>
2018-07-27 15:11:17 +0200

    set version to 2.0.4

--- VIENNAAddIn/Properties/AssemblyInfo.cs
+++ VIENNAAddIn/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion ("2.0.3.*")]
+[assembly: AssemblyVersion ("2.0.4.*")]
 [assembly: AssemblyDelaySign (false)]
 [assembly: AssemblyKeyFile ("")]
 [assembly: AssemblyKeyName ("")]
